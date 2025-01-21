12c97b8e407bf7a647ebf76758efedebd121957a
ikalisa <ikalisa1@alustudent.com>
2025-01-20 15:43:25 -0900

    results

--- matrix_mul.py
+++ matrix_mul.py
@@ -10,9 +10,9 @@ Ans = []
 
 for i in range(len(Prices)):
     row_sum = 0
-    for j in range(len(Prices[0])):
-        row_sum += Prices[i][j] * Array2[j]  
-    Ans.append(row_sum)  
 
+    for j in range(len(Prices[0])):          # Loop through each element in the current row
+        row_sum += Prices[i][j] * Array2[j]  # Calculate the product of the price and the corresponding quantity 
+    Ans.append(row_sum)                      # Append the calculated sum for the current row to the results list  
 
-print(Ans)
+print(Ans) 
